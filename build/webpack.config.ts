6361d787c361a7bdaf73b635bebb46e48646a953
Michael Shipper <Michae.Shipper@charter.com>
2018-11-27 14:28:18 -0600

    rolling in changes

--- build/webpack.config.ts
+++ build/webpack.config.ts
@@ -7,7 +7,6 @@ const args = require('minimist')(process.argv.slice(2));
 const isProduction = args && args.mode === 'production';
 const filename = isProduction ? '[name].min.js' : '[name].js';
 
-
 const config: webpack.Configuration = {
     devtool: isProduction ? 'source-map' : false,
     entry: {
@@ -15,54 +14,18 @@ const config: webpack.Configuration = {
     },
     module: {
         rules: [
-          {
-            test: /\.ts$/,
-            exclude: /node_modules/,
-            use: 
-             {
-              loader: "babel-loader",
-              options: {
-                 compact: isProduction,
-                 presets: [
-                   [
-                     "@babel/preset-env", {
-                       debug: true,
-                       //modules: "commonjs",
-                     }
-                   ],
-                   '@babel/preset-typescript',
-                 ],
-                 plugins: [
-                   "@babel/plugin-proposal-export-namespace-from",
-                   "@babel/plugin-transform-typescript",
-                   "@babel/plugin-syntax-typescript",
-                   "@babel/plugin-syntax-jsx",
-                   "@babel/plugin-transform-react-jsx",
-                   "@babel/plugin-transform-react-display-name",
-                   "@babel/plugin-transform-react-inline-elements",
-                   "@babel/plugin-proposal-class-properties",
-                   "@babel/plugin-transform-runtime",
-                   "@babel/plugin-transform-arrow-functions",
-                   "@babel/plugin-proposal-object-rest-spread"
-                 ]
-               }
-             }
-          },
-
-          {
-               enforce: 'pre',
-               exclude: /node_modules/,
-               test: /\.ts?$/,
-               use: 'tslint-loader',
-          },
-          {
-             exclude: /node_modules/,
-             test: /\.ts?$/,
-             use: 'ts-loader',
-          },
-
+            {
+                enforce: 'pre',
+                exclude: /node_modules/,
+                test: /\.ts?$/,
+                use: 'tslint-loader',
+            },
+            {
+                exclude: /node_modules/,
+                test: /\.ts?$/,
+                use: 'ts-loader',
+            },
         ],
-
     },
     output: {
         filename,
